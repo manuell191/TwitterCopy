de2ac3eebf3a9b0d64ca3332268890f8630ba30d
manuell191 <60369853+manuell191@users.noreply.github.com>
2022-10-15 17:42:17 -0700

    created database

--- TwitterCopy/twitterapp/models.py
+++ TwitterCopy/twitterapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "User's username is: " + self.username
+
+class Post(models.Model):
+    content = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username + " says " + self.content
