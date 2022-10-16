b51e6cf8895e864999ea25df09e538292777c53d
manuell191 <60369853+manuell191@users.noreply.github.com>
2022-10-15 17:56:34 -0700

    Commit

--- TwitterCopy/twitterapp/views.py
+++ TwitterCopy/twitterapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import User, Post
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+class UserList(ListView):
+    model = User
+    template_name = 'user_list.html'
